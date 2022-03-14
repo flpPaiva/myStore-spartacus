8f511b93ae686bf0d7c1cf3da862e30827971dbb
Felipe <felipecajon91@gmail.com>
2022-03-14 10:47:05 -0300

    Create randomProduct

--- src/app/sale/sale.component.ts
+++ src/app/sale/sale.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { CmsService, Page } from '@spartacus/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { RouterAction } from "@ngrx/router-store";
+import { CmsService, Page, ProductSearchService, RoutingService } from "@spartacus/core";
+import { SearchResults } from "@spartacus/storefront";
+import { Observable } from "rxjs";
+import { filter, pluck, take } from "rxjs/operators";
 
 @Component({
-  selector: 'app-sales',
-  templateUrl: './sale.component.html',
-  styleUrls: ['./sale.component.scss']
+  selector: "app-sales",
+  templateUrl: "./sale.component.html",
+  styleUrls: ["./sale.component.scss"],
 })
-
 export class SaleComponent implements OnInit {
-
   idComponents: Array<string> = [];
   components: Array<any> = [];
   currentPage: Page | undefined;
   $currentPage: Observable<Page> = this.cmsService.getCurrentPage();
-  constructor(private cmsService: CmsService) { }
+
+  constructor(
+    private cmsService: CmsService,
+    private routingService: RoutingService,
+    private productSearchService: ProductSearchService
+  ) {}
 
   ngOnInit(): void {
     this.$currentPage.subscribe((page: Page) => {
@@ -24,10 +30,22 @@ export class SaleComponent implements OnInit {
   }
 
   getComponents(page: Page) {
-    debugger
-    this.cmsService.getComponentData('SaleParagraphComponent').subscribe(data => {
-      debugger
-    })
+    this.cmsService
+      .getComponentData("SaleParagraphComponent")
+      .subscribe(console.log);
   }
 
+  goToRandomProduct() {
+    this.productSearchService.search('cam');
+    this.productSearchService.getResults()
+    .pipe(
+      filter(res => res.hasOwnProperty('products')),
+      pluck('products'),
+      take(1)
+    )
+    .subscribe((products : any) => {
+      const randomProduct = products[Math.floor(Math.random() * products.length)];
+      this.routingService.go({ cxRoute: "product", params: { code: randomProduct.code } });
+    }); 
+  }
 }
